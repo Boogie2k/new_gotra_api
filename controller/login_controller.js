cd3997fe319bde29efe24cca898260f7739d381f
Boogie2k <oghosabenjamin@gmail.com>
2023-12-01 06:53:50 +0100

    commit

--- controller/login_controller.js
+++ controller/login_controller.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
     let { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user || user.password !== password) {
-      res.json("invalid credentials");
+      res.status(401).res.json("invalid credentials");
       return;
     }
     const token = jwt.sign(
